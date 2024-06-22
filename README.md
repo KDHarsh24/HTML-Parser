0236f48f5e3f7e38ec0837fb88bc4368ad54a8cd
kdharsh24 <harshkumardas24@gmail.com>
2024-06-22 19:13:49 +0530

    Some Bug Fixes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HTML-Parser
 A Simple HTML Parser to compete with beautiful-soup
+
+Not designed to work for newly declared self closing tags
--- parser.py
+++ parser.py
@@ -26,8 +26,9 @@ def liveHtmlCode(url):
     stringLine = []
     for line in web_file:
         stringLine.append(str(line))
-    for i in range(len(stringLine)):
+    for i in range(len(stringLine) - 1):
         stringLine[i] = stringLine[i][2:len(stringLine[i])-3]
+    stringLine[-1] = stringLine[-1][2:len(stringLine[-1])-1]
 
     return stringLine
 
@@ -89,7 +90,6 @@ def extractInfofromTag(line):
                     valueAnswer += lines[i]
                 if no == 2:
                     break
-        
         return valueAnswer
 
 
@@ -109,46 +109,56 @@ def extractInfofromTag(line):
 
 def tagDefiners(allBlock):
     onGoingBerth = ["None"]
+    scriptrun = False
     selfClosingTags = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr', '!DOCTYPE', 'circle', '!--']
-    for items in allBlock[1:]:
-        tagElement = extractInfofromTag(items)
-        if tagElement == "Notag":
+    for items in allBlock[1:]: # reading each tag separated by allblocks
+        tagElement = extractInfofromTag(items) # storing each tags data
+        if tagElement == "Notag": # if no tag then its innercontent only
             if onGoingBerth[-1] != "None":
                 onGoingBerth[-1].innerHTML = onGoingBerth[-1].innerHTML + items # type: ignore
-        else:
-            if onGoingBerth[-1] != "None":   
+        else: # when an html element is found
+            if onGoingBerth[-1] != "None":  # when root is not entered
                 if tagElement['tagname'] == '/'+onGoingBerth[-1].tagData['tagname']: # type: ignore
+                    # when it's matching close tag
+                    scriptrun = False
                     head = onGoingBerth.pop()
-                    if onGoingBerth[-1] != "None":
+                    if onGoingBerth[-1] != "None": # adding to parent and childs
                         head.parent = onGoingBerth[-1] # type: ignore
                         onGoingBerth[-1].insertChild(head) # type: ignore
-                else:
-                    if tagElement['tagname'][0] == '/':
+                    else: # parent is none
+                        head.parent = "None" # type: ignore
+                else: # not a closing match tag
+                    if tagElement['tagname'][0] == '/' and scriptrun == False: # not a match tag but closing tag actually it's a error, curently resolving error
                         head = onGoingBerth.pop()
-                        head.tagData['Error'] = 1 # type: ignore
-                        onGoingBerth[-1].insertChild(head) # type: ignore
-                    else:
+                        if onGoingBerth[-1] != "None":
+                            onGoingBerth[-1].insertChild(head) # type: ignore
+                        head.tagData['Error'] = True # type: ignore
+                    else: # it's a starting normal tag
                         node = htmlNode(tagElement, "", onGoingBerth[-1])
-                        if tagElement['tagname'] not in selfClosingTags:
+                        if tagElement['tagname'] not in selfClosingTags and scriptrun == False: # not self closing tag,  most used in new tag encounter
                             onGoingBerth.append(node) # type: ignore
                         else:
-                            if tagElement['tagname'] != '!--':
+                            if tagElement['tagname'] != '!--' and scriptrun == False: # resolving selfclosing tag except the comment section as comment is also considered as self closing
                                 node.parent = onGoingBerth[-1]
                                 onGoingBerth[-1].insertChild(node) # type: ignore
-            else:
+                        if tagElement['tagname'] == 'script':
+                            scriptrun = True
+            else: # for entering root i.e. top none
                 node = htmlNode(tagElement, "", onGoingBerth[-1])
                 if tagElement['tagname'] not in selfClosingTags:
-                        onGoingBerth.append(node) # type: ignore
+                    if tagElement['tagname'] == 'script':
+                        scriptrun = True
+                    onGoingBerth.append(node) # type: ignore
                 else:
                     if tagElement['tagname'] != '!-- ':
                         node.parent = onGoingBerth[-1]
 
     return head
 
-htmlArray = readingFromLocal('C:/Users/Harsh/Desktop/Parser/a.html')
-# htmlArray = liveHtmlCode('http://127.0.0.1:5500/index.html')
+# htmlArray = readingFromLocal('C:/Users/Harsh/Desktop/HTML-Parser/h.html')
+htmlArray = liveHtmlCode('https://www.linkedin.com')
 # print(htmlArray)
 allBlocks = identifyTags(htmlArray)
+# print(allBlocks)
 a = tagDefiners(allBlocks)
 printTree(a, 0)
- # type: ignore
